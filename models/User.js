e4fd576607d254340d425b5219fee2cdc4afa934
zainabaliammarali-cloud <zainabaliammarali@gmail.com>
2026-07-21 05:10:01 +0300

    define user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,19 @@
 
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
