d123f833b2e1d78da21abe5d63c3952ab85152e4
zainabaliammarali-cloud <zainabaliammarali@gmail.com>
2026-07-22 16:17:15 +0300

    refactor

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
+const User = require("../models/User.js");
 const bcrypt = require("bcrypt");
 
 
